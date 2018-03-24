3afa8a177a97a5159c48fb7352449240c34af4e3
shrikalau <shrikala.upadhya@gmail.com>
2018-03-23 20:55:14 -0700

    added increment to sum valve

--- com.shrikala.git.newTrial/src/com/shrikala/git/newTrial/Trial.java
+++ com.shrikala.git.newTrial/src/com/shrikala/git/newTrial/Trial.java
@@ -2,10 +2,11 @@ package com.shrikala.git.newTrial;
 
 public class Trial {
 
-	public static void main() {
+	public static void main(String[] args) {
 		int a=10;
 		int b=20;
 		int sum=a+b;
-		System.out.println("The sum is"+sum);
+		System.out.println("The sum is "+sum);
+		System.out.println(sum+1);
 	}
 }
